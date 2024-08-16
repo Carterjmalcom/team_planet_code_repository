7e4c4d5e79ebd2c117ce102ee356d8f72f795a4b
Carterjmalcom <carterjmalcom@gmail.com>
2024-08-16 14:27:27 -0700

    final code

--- finalCode.js
+++ finalCode.js
@@ -1,34 +1,89 @@
 const prompt = require('prompt-sync')();
 
 const gravityFactors = require('./utils/earthGravityFactors.js');
+const alienGravityFactors = require('./utils/alienGravityFactors.js');
 
-function showUserFactors(type, value) {
+var typeInput
+var systemInput
+var valueInput
+var planetsInput
+
+function showUserFactors(type, system, value, planets) {
     let results = {}
     let measurment;
-    for (let planet in gravityFactors) { 
-        results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2))
+    if (planets == 1) {
+        for (let planet in gravityFactors) {
+            results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2))
+        }
+    } else {
+        for (let planet in alienGravityFactors) {
+            results[planet] = parseFloat((alienGravityFactors[planet] * value).toFixed(2))
+        }
     }
     switch (type) {
         case 'jump':
-            measurment = "cm"
+            if (system == "metric") {
+                measurment = "cm"
+            } else {
+                measurment = "in"
+            }
             break;
-        
+
         case 'weight':
-            measurment = "kg"
+            if (system == "metric") {
+                measurment = "kg"
+            } else {
+                measurment = "lbs"
+            }
+            break;
+        case 'pushups':
+            measurment = " repetitions"
             break;
 
         default:
             break
     }
-    for (let planet in results) { 
+    for (let planet in results) {
         console.log(`Your ${type} on ${planet} is ${results[planet]}${measurment}.`)
     }
-    
+
 }
 function getUserInput() {
-    typeInput = prompt("Enter Type> ")
-    valueInput = prompt("Enter Value> ")
-    showUserFactors(typeInput, valueInput)
+    while (true) {
+        typeInput = prompt("Enter Type (jump, weight, pushups >> ").trim().toLowerCase()
+        if (typeInput === "jump" || typeInput === "weight" || typeInput === "pushups") {
+
+            break
+        }
+        else {
+            console.log('Retry')
+        }
+    }
+    while (true) {
+        if (typeInput === "pushups") {
+            break
+        }
+        systemInput = prompt("Enter System (metric, imperial) >> ").trim().toLowerCase()
+        if (systemInput === "metric" || systemInput === "imperial") {
+            break
+        }
+        else {
+            console.log('Retry')
+        }
+
+    }
+    while (true) {
+        valueInput = prompt("Enter Value (amount of unit) >> ")
+        if (!isNaN(valueInput)) {
+            break
+        }
+        else {
+            console.log('Retry')
+        }
+
+    }
+    planetsInput = prompt("Enter Solar System (1, 2) >> ")
+    showUserFactors(typeInput, systemInput, valueInput, planetsInput)
 
 }
 
